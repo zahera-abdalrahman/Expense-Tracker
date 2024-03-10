9cd9cdd5dc8cae48ac16b418067eaf05a1a1380c
zahera-abdalrahman <zaheraalakash15@gmail.com>
2024-03-10 23:21:01 +0300

    Models Initialization

---
+++ Expense Tracker/Expense Tracker/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Transaction> Transaction { get; set; }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ Expense Tracker/Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //Category
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string? Note { get; set; }
+
+        public int Amount{ get; set; }
+
+        public DateTime Date { get; set; }=DateTime.Now;
+
+    }
+}
